9a632f9f37dd0d171d2990ff569a0048a45b5086
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2020-07-16 22:41:54 +0700

    feat: add console struct implementation

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"git-merge-example/console"
 	"git-merge-example/controller"
 )
 
@@ -9,4 +10,13 @@ func main() {
 	var newController controller.Controller
 	newController = controller.Square
 	fmt.Println(newController)
+
+	ps5 := console.Console{
+		ID:   1,
+		Name: "PS5",
+	}
+
+	fmt.Println("========  ========")
+	fmt.Printf("%+v\n", ps5)
+	fmt.Println("=================")
 }
